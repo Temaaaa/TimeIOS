59161aeeebfd9cf895b299827e54e1e2381c646c
Tema <Ledchak1@gmail.com>
2022-04-20 10:27:15 +0300

    1

--- iTimer/ViewController.swift
+++ iTimer/ViewController.swift
@@ -14,15 +14,49 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var stopWatch = Timer()
     var counter: Double = 0.0
     var lapArray: [String] = []
+    var isPlay: Bool = false
     
     @IBOutlet weak var tableLap: UITableView!
-    @IBAction func activateTimer(_ sender: UIButton) {
-        stopWatch = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(tikTok), userInfo: nil, repeats: true)
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBAction func activateTimer(_ sender: UIButton){
+        isPlay = !isPlay
+        if isPlay {
+            stopWatch = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(tikTok), userInfo: nil, repeats: true)
+            let image = UIImage(systemName: "pause.fill")
+            sender.setImage(image, for: .normal)
+            sender.setTitle("Pause", for: .normal)
+            let imageReset = UIImage(systemName: "plus.rectangle.fill")
+            resetButton.setImage(imageReset, for: .normal)
+            resetButton.setTitle("Lap", for: .normal)
+            resetButton.isEnabled = true
+           
+            
+        } else {
+            stopWatch.invalidate()
+            let image = UIImage(systemName: "play.fill")
+            sender.setImage(image, for: .normal)
+            sender.setTitle("Start", for: .normal)
+            let imageReset = UIImage(systemName: "trash.fill")
+            resetButton.setImage(imageReset, for: .normal)
+            resetButton.setTitle("Reset", for: .normal)
+        }
+        
     }
     
     @IBAction func addLap(_ sender: UIButton) {
+        if isPlay{
         lapArray.append(timerOutput.text!)
         tableLap.reloadData()
+            
+        } else {
+            counter = 0.0
+            timerOutput.text = "00:00"
+            lapArray.removeAll()
+            tableLap.reloadData()
+            sender.isEnabled = false
+        }
+        
     }
     
     @objc func tikTok() {
@@ -58,11 +92,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel!.text = lapArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! row
+        
+        cell.rowLabel.text = lapArray[indexPath.row]
         return cell
     }
 
 
+}
+
+class row: UITableViewCell {
+    
+    @IBOutlet weak var rowLabel: UILabel!
+    
 }
 
